1d9a33710964b62d239475af0fb1fd06a7ae5c21
Jim Tang <wxfreakk91@gmail.com>
2024-08-22 02:14:36 -0500

    initial commit with working map

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import Map from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
+
+export default function App() {
+  return (
+    <Map
+      initialViewState={{
+        longitude: -98.5556,
+        latitude: 39.8097,
+        zoom: 4,
+      }}
+      style={{
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        left: 0,
+        top: 0,
+      }}
+      // onRender={(event) => event.target.resize()}
+      minZoom={4}
+      maxZoom={10}
+      touchPitch={false}
+      mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+    />
+  );
+}
