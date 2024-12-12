c12c34d27525adabc7d5831059eda91de2b76e48
Jim Tang <wxfreakk91@gmail.com>
2024-12-11 21:42:04 -0600

    add interactivity and datatables

--- src/soundings/SoundingModal.tsx
+++ src/soundings/SoundingModal.tsx
@@ -1,7 +1,17 @@
 import { Flex, Modal } from '@mantine/core';
 import { useChaseCaseStore } from '../store';
-import { Hodograph, ProfileProvider, SkewT } from 'upperair';
+import {
+  Hodograph,
+  KinematicTable,
+  LayersTable,
+  ParametersTable,
+  ParcelTable,
+  ProfileProvider,
+  SelectedPointsTable,
+  SkewT,
+} from 'upperair';
 import { useGetSounding } from './api';
+import styles from './SoundingModal.module.css';
 
 export function SoundingModal({ isDesktop }: { isDesktop: boolean }) {
   const [soundingCaseId, setSoundingCaseId] = useChaseCaseStore((state) => [
@@ -18,19 +28,40 @@ export function SoundingModal({ isDesktop }: { isDesktop: boolean }) {
   }
 
   return (
-    <Modal opened={!!soundingCaseId} onClose={onClose} size='90%'>
-      <ProfileProvider profile={data.data}>
-        <Flex direction={isDesktop ? 'row' : 'column'} bg='dark'>
-          <SkewT>
-            <SkewT.ReferenceLines />
-            <SkewT.ProfileData showParcel='ML' />
-          </SkewT>
-          <Hodograph>
-            <Hodograph.ReferenceLines />
-            <Hodograph.ProfileData />
-          </Hodograph>
-        </Flex>
-      </ProfileProvider>
+    <Modal opened={!!soundingCaseId} onClose={onClose} size='85%'>
+      <Flex direction='column' bg='dark'>
+        <ProfileProvider profile={data.data}>
+          <Flex direction={isDesktop ? 'row' : 'column'}>
+            <SkewT interactive mode='pan'>
+              <SkewT.ReferenceLines />
+              <SkewT.ProfileData showParcel='ML' />
+            </SkewT>
+            <Hodograph interactive mode='pan'>
+              <Hodograph.ReferenceLines />
+              <Hodograph.ProfileData />
+            </Hodograph>
+          </Flex>
+          <Flex
+            className={styles.datatables}
+            direction={isDesktop ? 'row' : 'column'}
+          >
+            <ParcelTable />
+            <KinematicTable />
+          </Flex>
+          <Flex
+            className={styles.datatables}
+            direction={isDesktop ? 'row' : 'column'}
+          >
+            <Flex direction='column' flex={1}>
+              <LayersTable />
+              <ParametersTable />
+            </Flex>
+            <Flex flex={2}>
+              <SelectedPointsTable />
+            </Flex>
+          </Flex>
+        </ProfileProvider>
+      </Flex>
     </Modal>
   );
 }
